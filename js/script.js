b552afdf0a4474f61ac5416198c4bede63652d7f
caitmh <churkes@gmail.com>
2017-06-23 17:48:39 -0600

    Friday-progress

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+	$('.about-button').click(function() {
+		$('.section-titles').addClass('typing-animation');
+	})
+
+	$('.work-button').click(function() {
+		$('.section-titles').addClass('typing-animation');
+	})
+
+	$('.contact-button').click(function() {
+		$('.section-titles').addClass('typing-animation');
+	})
+
+	$('.insite-link').click(function() {
+		$('.section-titles').addClass('typing-animation');
+	})
+
+});
