5e3f4d6194b2c411477278d2239924b74947e260
caitmh <churkes@gmail.com>
2017-06-26 18:37:32 -0600

    Semi-final-draft

--- js/script.js
+++ js/script.js
@@ -1,19 +1,61 @@
 $(document).ready(function() {
 
-	$('.about-button').click(function() {
-		$('.section-titles').addClass('typing-animation');
-	})
+	$('.about-button').on("click", function(){
+        
+        var targets = $('.section-titles');
+        for(var i=0; i < targets.length; i++){
+            console.log(targets[i]);
+            // -> removing the class
+            targets[i].classList.remove("typing-animation");
+        
+            void targets[i].offsetWidth;
+         
+            // -> and re-adding the class
+            targets[i].classList.add("typing-animation");
+        }
+    });
 
-	$('.work-button').click(function() {
-		$('.section-titles').addClass('typing-animation');
-	})
 
-	$('.contact-button').click(function() {
-		$('.section-titles').addClass('typing-animation');
-	})
+    $('.work-button').on("click", function(){
+        var targets = $('.section-titles');
+        for(var i=0; i < targets.length; i++){
+            console.log(targets[i]);
+            // -> removing the class
+            targets[i].classList.remove("typing-animation");
+        
+            void targets[i].offsetWidth;
+         
+            // -> and re-adding the class
+            targets[i].classList.add("typing-animation");
+        }
+    });
 
-	$('.insite-link').click(function() {
-		$('.section-titles').addClass('typing-animation');
-	})
+    $('.contact-button').on("click", function(){
+        var targets = $('.section-titles');
+        for(var i=0; i < targets.length; i++){
+            console.log(targets[i]);
+            // -> removing the class
+            targets[i].classList.remove("typing-animation");
+        
+            void targets[i].offsetWidth;
+         
+            // -> and re-adding the class
+            targets[i].classList.add("typing-animation");
+        }
+    });
+
+    $('.insite-link').on("click", function(){
+        var targets = $('.section-titles');
+        for(var i=0; i < targets.length; i++){
+            console.log(targets[i]);
+            // -> removing the class
+            targets[i].classList.remove("typing-animation");
+        
+            void targets[i].offsetWidth;
+         
+            // -> and re-adding the class
+            targets[i].classList.add("typing-animation");
+        }
+    });
 
 });
